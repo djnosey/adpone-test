6e241635b21370dae15ef8d961dbce9339930625
steven payne <djnosey@hotmail.co.uk>
2021-02-22 10:37:48 +0100

    test complete

---
+++ test.js
@@ -0,0 +1,20 @@
+const myStr = "aaaabbbbbcca";
+
+const myFunc = (str) => {
+  let myArr = [...str];
+  let results = [];
+  let count = 1;
+
+  myArr.forEach((item, index) => {
+    if (item === myArr[index + 1]) {
+      count++;
+    } else {
+      results.push([item, count]);
+      count = 1;
+    }
+  });
+
+  return results;
+};
+
+
